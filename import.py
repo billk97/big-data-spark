508ad3da02337840d3753728c868dfe382375227
VasileosK <vasileios@konstantinou.dev>
2020-12-03 00:50:49 +0200

    successfull import of txt to tables

--- import.py
+++ import.py
@@ -1,14 +1,13 @@
 from pyspark import SparkConf, SparkContext
-from pyspark.sql import SparkSession, types
+from pyspark.sql import SparkSession, types, HiveContext
 from pyspark.sql.types import *
+from pyspark.sql.functions import to_date
 
 spark = SparkSession.builder.appName("import").config("spark.some.config.option", "some-value").getOrCreate()
-spark.sparkContext.setLogLevel("")
+spark.sparkContext.setLogLevel("WARN")
 sc = spark.sparkContext 
 
 borrowers_txt = sc.textFile("BORROWERS.TXT")
-loans_txt = sc.textFile("LOANS.TXT")
-
 borrowers = borrowers_txt.coalesce(1).map(lambda line: line.split("|"))
 headers = borrowers.first()
 borrowers = borrowers.filter(lambda line: line != headers)
@@ -20,24 +19,36 @@ borrowers_schema = StructType([
     StructField(headers[2], StringType(), True)
 ])
 
+borrowers_DF = spark.createDataFrame(borrowers, borrowers_schema)
+borrowers_DF.createOrReplaceTempView("borrowersTable")
+query = spark.sql("""SELECT * FROM borrowersTable """)
+
+query.write.format("csv").save("borrowersTable")
+
+loans_txt = sc.textFile("LOANS.TXT")
 loans = loans_txt.coalesce(1).map(lambda line: line.split("|"))
 headers = loans.first()
 loans = loans.filter(lambda line: line != headers)
-loans = loans.map(lambda line: (int(line[0]), line[1], int(line[2]), line[3]))
+loans = loans.map(lambda line: (int(line[0]),
+                                line[1],
+                                int(line[2]),
+                                line[3]
+                                ))
 
 # last value is true means it's allowed to be null
 loans_schema = StructType([
     StructField(headers[0], IntegerType(), True),
     StructField(headers[1], StringType(), True),
     StructField(headers[2], IntegerType(), True),
-    StructField(headers[3], TimestampType(), True)
+    StructField(headers[3], StringType(), True)
 ])
 
-borrowers_DF = spark.createDataFrame(borrowers, borrowers_schema)
-borrowers_DF.createOrReplaceTempView("borrowersTable")
-query = spark.sql("""SELECT * FROM borrowersTable """)
+loans_DF = spark.createDataFrame(loans, loans_schema)
+loans_DF.createOrReplaceTempView("loansTable")
+query = spark.sql("""SELECT * FROM loansTable""")
+query.write.format("csv").save("loansTable")
 
-query.write.format("csv").save("borrowersTable")
+
 
 # borrower_df = borrowers.toDF()
 # borrower_df.write.csv("borrowerCSV")
